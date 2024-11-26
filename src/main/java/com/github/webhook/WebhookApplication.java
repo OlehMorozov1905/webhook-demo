4da303a823f968e40ece8dd5b610e95e40ec2318
OlehMorozov1905 <olegmorozov140988@gmail.com>
2024-11-26 14:30:08 +0100

    webhook-demo commit-2

--- src/main/java/com/github/webhook/WebhookApplication.java
+++ src/main/java/com/github/webhook/WebhookApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebhookApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(WebhookApplication.class, args);
     }
 }
